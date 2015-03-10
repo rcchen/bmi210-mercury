dad8ea8feab9ab546895b9f31e04552510e4ef35
Ryan Perez <rrperez@stanford.edu>
2015-03-09 23:34:01 -0700

    wrote full functionality, but hasn't been tested

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -23,35 +23,42 @@ class MessagesController < ApplicationController
 
       # Check to see if there has been a message
       if last_log
+          body = body.downcase
+          if body == "yes" or body == "y" or body == "no" or body == "n" 
+            last_log.answer = body
 
-          last_log.answer = body
 
-          # Finds the next message to be sent according to the condtion
-          message = Message.find(last_log.message_id)
-          messages = Message.where(condition: message.condition)
-          if messages.index(message) < messages.count - 1
-            next_message = messages[messages.index(message) + 1]
-            send_message phone, next_message.message
-            Log.create(:message_id => next_message.id, :user_id => user.id)
+            # Finds the next message to be sent according to the condtion
+            message = Message.find(last_log.message_id)
+            messages = Message.where(condition: message.condition)
+            if messages.index(message) < messages.count - 1
+              next_message = messages[messages.index(message) + 1]
+              send_message phone, next_message.message
+              Log.create(:message_id => next_message.id, :user_id => user.id)
+            else
+              send_message phone, recommendation(user)
           else
-            calculate_result
+            send_message phone, "Please enter either yes or no."
+          end 
         end
 
       # User has just registered so there is no last log
       else
         # Rotate through missing information
         if user.age.nil?
-          user.age = Integer(body)
-          user.save
-          send_message phone, "Which condition would you like to test?"
+          if body.is_a?(Integer)
+            user.age = Integer(body)
+            user.save
+            send_message phone, "Which condition would you like to test?"
+          else
+            send_message phone, "Please enter a valid age."
         else
-          if body == "MI" or "Pneumonia" or "Diarrheal Disease"
-
+          if body == "MI" or "Pneumonia" or "Diarrhea"
             messages = Message.where(condition: body)
             send_message phone, messages.first.message
             Log.create(:message_id => messages.first.id, :user_id => user.id)
           else
-            # Prompt for the condition again
+            send_message phone, "Please enter either MI, Pneumonia, or Diarrhea."
           end
         end
       end
@@ -71,6 +78,20 @@ class MessagesController < ApplicationController
     client = Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
     message = client.account.messages.create(:body => body, :to => number, :from => ENV["TWILIO_NUM"])
     return message.sid
+  end
+
+  def recommendation(user)
+    logs = Log.where(user_id: user.id)
+    for log in logs
+      if log.answer == "yes" total_weight += log.message.weight
+
+      end
+    end
+    if total_weight >= logs.first.message.factor.condition.threshold
+      "Your total weight is " + total_weight + " and the threshold is " + logs.first.message.factor.condition.threshold + ". We recommend that you visit a hospital."
+    else
+      "Your total weight is " + total_weight + " and the threshold is " + logs.first.message.factor.condition.threshold + ". We do not recommend that you visit a hospital."
+    end
   end
 
 end
--- app/models/condition.rb
+++ app/models/condition.rb
@@ -3,4 +3,6 @@ class Condition < ActiveRecord::Base
 
   validates :name, presence: true, allow_blank: false
   validates :hospital_treatment, presence: true, allow_blank: false
+
+  # add threshold
 end
