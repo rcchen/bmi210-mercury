a0074e3f040a733ce8647c2e23b3090805f0bfa4
Roger Chen <rogerthechen@gmail.com>
2015-03-11 13:19:51 -0700

    Rolling back messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,53 +15,33 @@ class MessagesController < ApplicationController
     # Create a user if they do not currently exist
     if user.nil?
       user = User.create(:phone => phone)
-      send_message phone, "What is your age?"
-
-    else
-      # Retrieve the last message sent to the user using logs
-      last_log = user.logs.last
-
-      # Check to see if there has been a message
-      if last_log
-          body = body.downcase
-          if body == "yes" or body == "y" or body == "no" or body == "n" 
-            last_log.answer = body
-            # Finds the next message to be sent according to the condtion
-            message = Message.find(last_log.message_id)
-            messages = Message.where(condition: message.condition)
-            if messages.index(message) < messages.count - 1
-              next_message = messages[messages.index(message) + 1]
-              send_message phone, next_message.message
-              Log.create(:message_id => next_message.id, :user_id => user.id)
-            else
-              send_message phone, recommendation(user)
-            end
-          else
-            send_message phone, "Please enter either yes or no."
-          end 
-        end
-      # User has just registered so there is no last log
-      else
-        # Rotate through missing information
-        if user.age.nil?
-          if body.is_a?(Integer)
-            user.age = Integer(body)
-            user.save
-            send_message phone, "Which condition would you like to test?"
-          else
-            send_message phone, "Please enter a valid age."
-          end
-        else
-          if body == "MI" or "Pneumonia" or "Diarrhea"
-            messages = Message.where(condition: body)
-            send_message phone, messages.first.message
-            Log.create(:message_id => messages.first.id, :user_id => user.id)
-          else
-            send_message phone, "Please enter either MI, Pneumonia, or Diarrhea."
-          end
-        end
-      end
     end
+
+    # Retrieve the last message sent to the user using logs
+    last_log = user.logs.last
+
+    # Check to see if there has been a message
+    if last_log
+
+      # Rotate through missing information
+      if last_log.message_id == 1
+        user.age = Integer(body)
+        user.save
+        Log.create(:message_id => 2, :user_id => user.id)
+      end
+
+    # User has just registered so there is no last log
+    else
+      Log.create(:message_id => 1, :user_id => user.id)
+    end
+
+    # Rotate through missing information
+    if user.age.nil?
+      send_message phone, "What is your age?"
+    else
+      send_message phone, "Your age is #{user.age}"
+    end
+
     render nothing: true
   end
 
@@ -72,19 +52,5 @@ class MessagesController < ApplicationController
     message = client.account.messages.create(:body => body, :to => number, :from => ENV["TWILIO_NUM"])
     return message.sid
   end
-
-  # def recommendation(user)
-  #   logs = Log.where(user_id: user.id)
-  #   for log in logs
-  #     if log.answer == "yes"
-  #       total_weight += log.message.weight
-  #     end
-  #   end
-  #   if total_weight >= logs.first.message.factor.condition.threshold
-  #     "Your total weight is " + total_weight + " and the threshold is " + logs.first.message.factor.condition.threshold + ". We recommend that you visit a hospital."
-  #   else
-  #     "Your total weight is " + total_weight + " and the threshold is " + logs.first.message.factor.condition.threshold + ". We do not recommend that you visit a hospital."
-  #   end
-  # end
 
 end
